170b542d796d40283c25693707bc202a5d89ddc1
Jeff Fermon <jfermon22@gmail.com>
2017-07-23 09:36:51 -0700

    Added info disclosure button

--- GamePlayViewController.swift
+++ GamePlayViewController.swift
@@ -15,38 +15,58 @@ class GamePlayViewController: UIViewController {
     var currentWord : ClueWord?
     
     @IBOutlet var wordLabel: UILabel!
+    @IBOutlet var nextButton: UIButton!
+    @IBOutlet var infoButton: UIButton!
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        updateToNextWord("viewdidload")
+        updateToNextWord("viewDidLoad")
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     @IBAction func updateToNextWord(_ sender: Any) {
         getNextWord()
-        print("nextword is \(currentWord)")
-        wordLabel.text = currentWord?.word;
-
+        print("nextWord is \(currentWord)")
+        
+        if let theSender = sender as? String
+        {
+            if theSender == "viewDidLoad" {
+                setUpForStart()
+            }
+        } else {
+            infoButton.isHidden = false;
+            wordLabel.text = currentWord?.word;
+        }
+        
     }
     
     func getNextWord() {
         currentWord = clueManager?.getNextClue(type: clueType!)
     }
+    
+    func setUpForStart() {
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        infoButton.isHidden = true;
+        wordLabel.text = "Press Start to Begin";
+        nextButton.setTitle("Start", for: .normal)
     }
-    */
-
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
